24decb5d9102c282d048532999c19ed51de234b9
dc820 <dc820@scarletmail.rutgers.edu>
2016-10-25 13:39:20 -0400

    Serializable WepDef Class/ Gen Dict. for WepDef/ Function Delagate to Fire

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -15,6 +15,11 @@ public class Hero : MonoBehaviour
 
     public Bounds bounds;
 
+    // Declare a new delegate type WeaponFireDelegate
+    public delegate void WeaponFireDelegate();
+    // Create a WeaponFireDelegate field named fireDelegate.
+    public WeaponFireDelegate fireDelegate;
+
     void Awake()
     {
         S = this; // Set the Singleton
@@ -42,6 +47,14 @@ public class Hero : MonoBehaviour
 
         // Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+
+        // Use the fireDelegate to fire Weapons
+        // First, make sure the Axis("Jump") button is pressed
+        // Then ensure that fireDelegate isn't null to avoid an error
+        if (Input.GetAxis("Jump") == 1 && fireDelegate != null)
+        {
+            fireDelegate();
+        }
     }
 
     // This variable holds a reference to the last triggering GameObject
